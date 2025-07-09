edf206cd9c91957c6adaaebab665dea2c4650828
Khushansh <khushanshbaliyan@gmail.com>
2025-07-09 19:08:58 +0530

    initialized variables

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,10 @@
+const api:string =  'https://api.dictionaryapi.dev/api/v1/entries/en/<word name>';
+//initialization of variables
+const inputBar = document.querySelector('#wordInput') as HTMLInputElement;
+const submitBtn =  document.querySelector('button') as HTMLButtonElement;
+const wordText = document.querySelector('#wordText') as HTMLElement;
+const phonetic = document.querySelector('#wordPhonetic') as HTMLElement;
+const pos = document.querySelector('#wordPOS') as HTMLElement;
+const wordSynonyms = document.querySelector('#wordSynonyms') as HTMLElement;
+
 
