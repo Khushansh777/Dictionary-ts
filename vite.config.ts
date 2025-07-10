f1c9e050a59263e796423f2a866ca344b8fdccbc
Khushansh <khushanshbaliyan@gmail.com>
2025-07-10 22:44:36 +0530

    fixed 404 error with main.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/Dictionary-ts/', // trailing slash is important
+    base: '/dictionart-ts/', // Match your repository name exactly
 });
